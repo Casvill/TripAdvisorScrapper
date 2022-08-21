619bfbe5b51a3bfb8ea3350f8469c9c60f89432d
rosaarz <63681015+rosaarz@users.noreply.github.com>
2022-08-21 14:06:50 -0500

    Upload 90 restaurant info

--- __main__.py
+++ __main__.py
@@ -1,30 +1,39 @@
 import extract
 import transform
 import pandas as pd
+import time 
 
+total_pages = 54 
+cities = 3
 
-
-for i in range(1,4):
+# city list
+for i in range(1,total_pages):
     soup,page = extract.extract_cities(i)
-    df_cities = transform.transform_cities( soup,page )
-    print('1')
+    df_cities = transform.transform_cities(soup,page)
+    time.sleep(0.1)
+print('\nLista de ciudades cargada')
 
 
-for href in df_cities['href'][:4]:
-    soup,page = extract.extract_restaurants_name(href)
-    df_restaurants = transform.transform_restaurants_name(soup,page,df_cities)
-    print('2')
+# restaurants per city
+for href in df_cities['href'][:cities]:
+    #print('href:',href)
+    soup,page = extract.extract_restaurant_names(href)
+    df_restaurants = transform.transform_restaurant_names(soup,page,df_cities)
+    time.sleep(0.5)
+print('\nLista de restaurantes por ciudad cargada')
 
-for i in range(2):
+
+# restaurant info
+for i in range(30*cities):
     href = df_restaurants['href'][i]
     restaurant = df_restaurants['restaurant'][i]
     city = df_restaurants['city'][i]
     state =df_restaurants['state'][i]
+    df_restaurants_info = transform.transform_restaurants_info(extract.extract_restaurant_info(href), restaurant, city, state)
+    time.sleep(1)
+print('\nInformación de restaurantes completa')
 
-    df_restaurants_info = transform.transform_restaurants_info(extract.extract_restaurants_info(href), restaurant, city, state)
-    print('3')
 
-
-df_cities.to_csv('cities.csv', index=False)
-df_restaurants.to_csv('restaurants.csv', index=False)
-df_restaurants_info.to_csv('restaurants_info.csv', index=False)
+df_cities.to_csv('Data/cities.csv', index=True)
+df_restaurants.to_csv('Data/restaurants.csv', index=True)
+df_restaurants_info.to_csv('Data/restaurants_info.csv', index=True)
--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 #USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:103.0) Gecko/20100101 Firefox/103.0'
-HEADERS = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:103.0) Gecko/20100101 Firefox/103.0'}
+HEADERS = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'}
 URL = 'https://www.tripadvisor.com.mx'
-CHROMEDRIVER_PATH = 'chromedriver.exe'
+CHROMEDRIVER_PATH = r"/Users/rosaarzabala/Documents/Projects/TripAdvisorScrapper/chromedriver"
